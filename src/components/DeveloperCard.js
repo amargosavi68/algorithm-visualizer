f1726432d00a519b67a0d09541b19a9c4b24c633
Amar Gosavi <gosaviamar681999@gmail.com>
2021-08-21 17:35:14 +0530

    developers card designed

--- src/components/DeveloperCard.js
+++ src/components/DeveloperCard.js
@@ -1,16 +1,73 @@
-import { Card, CardContent } from '@material-ui/core';
-import React from 'react'
+import {
+     Card,
+     CardContent,
+     CardHeader,
+     CardMedia,
+     CardActions,
+     Typography,
+     IconButton,
+     makeStyles,
+} from "@material-ui/core";
+import { LinkedIn, Mail, Twitter } from "@material-ui/icons";
+import React from "react";
+
+const useStyle = makeStyles((theme) => ({
+     card: {
+          position: "relative",
+          margin: "20px 0px",
+          width: "350px",
+          height: "auto",
+     },
+     cardMedia: {
+          height: 0,
+          paddingTop: "56.25%",
+     },
+     cardAction: {
+          position: "relative",
+          float: "right",
+     },
+     linkedIn: {
+          color: "#0000ff",
+     },
+     mail: {
+          color: "#000",
+     },
+     twitter: {
+          color: "#00ffff",
+     },
+     iconSize: {
+          fontSize: "30px",
+     },
+}));
 
 const DeveloperCard = (props) => {
      const { developer } = props;
+     const classes = useStyle();
 
      return (
-          <Card>
+          <Card className={classes.card}>
+               <CardHeader title={developer.name} subheader={developer.designation} />
+               <CardMedia
+                    className={classes.cardMedia}
+                    title={developer.name}
+                    image={developer.image}
+               />
                <CardContent>
-                    { developer.name }
+                    <Typography paragraph>{developer.description}</Typography>
                </CardContent>
+               <CardActions className={classes.cardAction}>
+                    <IconButton className={classes.mail}>
+                         <Mail className={classes.iconSize} />
+                    </IconButton>
+                    <IconButton className={classes.linkedIn}>
+                         <LinkedIn className={classes.iconSize} />
+                    </IconButton>
+                    <IconButton className={classes.twitter}>
+                         <Twitter className={classes.iconSize} />
+                    </IconButton>
+               </CardActions>
           </Card>
-     )
-}
+     );
+};
 
 export default DeveloperCard;
--- src/components/Developers.js
+++ src/components/Developers.js
@@ -1,16 +1,41 @@
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import { devlopersInfo } from "../lib/constant";
 import DeveloperCard from "./DeveloperCard";
 
+const useStyle = makeStyles((theme) => ({
+     container: {
+          minHeight: "90vh",
+          display: "flex",
+          justifyContent: "center",
+          alginItems: "center",
+          backgroundColor: "#f2f2f2",
+          marginTop: "10px",
+     },
+     grid: {
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alginItems: "center",
+     },
+}));
+
 const Developers = () => {
+     const classes = useStyle();
      return (
           <React.Fragment>
-               <Container>
-                    <Grid container>
+               <Container className={classes.container}>
+                    <Grid className={classes.grid}>
                          {devlopersInfo.map((developer) => {
                               return (
-                                   <Grid key={developer.name} item xs={12} sm={9} md={6} lg={3}>
+                                   <Grid
+                                        key={developer.name}
+                                        item
+                                        xs={12}
+                                        sm={9}
+                                        md={6}
+                                        className={classes.grid}
+                                   >
                                         <DeveloperCard developer={developer} />
                                    </Grid>
                               );
